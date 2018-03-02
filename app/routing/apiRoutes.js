4f16978c9ba642da4f463c93e3a39a4d100e9ada
Enas Siddiqi <enassiddiqi@Enass-MacBook-Pro.local>
2018-03-02 14:25:11 -0600

    Work on submitting array as int

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 
 // ===============================================================================
 // ROUTING
@@ -14,14 +14,18 @@ module.exports = function(app) {
     // API GET Requests
     // In each of the below cases when a user visits a link they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
-    app.get('../api/friends', function(req, res){
+    app.get('/api/friends', function(req, res){
         res.json(friendsData);
     });
 
     // API POST Requests
     // Submit survey data to server
-    app.post('../api/friends'), function(req, res){
+    app.post('/api/friends', function(req, res){
         friendsData.push(req.body);
-    }
+    });
 
+    app.post("/api/clear", function() {
+        // Empty out the arrays of data
+        friendsData = [];
+      });
 };
