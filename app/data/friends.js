5cea85e31b6ca74fa3837686298f1a5a2ade1c91
Enas Siddiqi <enassiddiqi@Enass-MacBook-Pro.local>
2018-03-05 13:04:41 -0600

    Working on it

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,100 @@
 var friendsArray = [
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Homer",
+        "photo": "https://vignette.wikia.nocookie.net/characters/images/b/bd/Homer_Simpson.png/revision/latest?cb=20170821194559",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Jerry",
+        "photo": "https://media1.fdncms.com/metrotimes/imager/u/original/4849516/screen_shot_2017-07-14_at_10.32.33_am.png",
+        "scores": [
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Miss Piggy",
+        "photo": "http://pbs.twimg.com/profile_images/662349173467975680/UxlUTQL0_400x400.jpg",
+        "scores": [
+            5,
+            4,
+            4,
+            4,
+            3,
+            1,
+            2,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Edward",
+        "photo": "https://vignette.wikia.nocookie.net/twilightsaga/images/6/69/Edward_Cullen.jpg/revision/latest?cb=20100803045949",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Key",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2
+        ]
+    },
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,23 +10,53 @@ var friendsData = require("../data/friends.js");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
     // API GET Requests
     // In each of the below cases when a user visits a link they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
-    app.get('/api/friends', function(req, res){
+    app.get('/api/friends', function (req, res) {
         res.json(friendsData);
     });
 
     // API POST Requests
     // Submit survey data to server
-    app.post('/api/friends', function(req, res){
+    app.post('/api/friends', function (req, res) {
+        // grab new user's scores to compare with friends in friendsData
+        var currentUserScores = req.body["scores[]"];
+        var scoresDiffArr = [];
+        var bestMatchIndex = 0;
+        console.log(friendsData);
+
+
+        //runs through all current friends in list
+        for (var i = 0; i < friendsData.length; i++) {
+            var scoresDiff = 0;
+            // Compare friends in friendsData with currentUser
+            for (var j = 0; j < currentUserScores.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(currentUserScores[j])));
+            }
+            //push results into scoresDiffArr
+            scoresDiffArr.push(scoresDiff);
+        }
+
+        //after all friends are compared, find best match
+        for (var i = 0; i < scoresDiffArr.length; i++) {
+            if (scoresDiffArr[i] <= scoresDiffArr[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        //return bestMatch data
+        var bestMatch = friendsData[bestMatch];
+        res.json(bestMatch);
+        console.log(bestMatch);
+        //pushes new submission into the friendsData array
         friendsData.push(req.body);
-        console.log(req.body);
     });
 
-    app.post("/api/clear", function() {
-        // Empty out the arrays of data
-        friendsData = [];
-      });
+app.post("/api/clear", function () {
+    // Empty out the arrays of data
+    friendsData = [];
+});
+
 };
